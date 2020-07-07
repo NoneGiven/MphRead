461d09d1290b87251bf0b12d771889fa23d5c928
None Given <none11given@gmail.com>
2020-07-07 01:07:44 -0400

    More cleanup.

--- src/MphRead/Renderer.cs
+++ src/MphRead/Renderer.cs
@@ -752,7 +752,7 @@ namespace MphRead
                 uint alpha = (uint)MathF.Round(mesh.OverrideColor.Value.Alpha / 255f * pixel.Alpha);
                 pixels.Add((red << 0) | (green << 8) | (blue << 16) | (alpha << 24));
             }
-            GL.BindTexture(TextureTarget.Texture2D, 99999);
+            GL.BindTexture(TextureTarget.Texture2D, Int32.MaxValue);
             GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, texture.Width, texture.Height, 0,
                 PixelFormat.Rgba, PixelType.UnsignedByte, pixels.ToArray());
         }
