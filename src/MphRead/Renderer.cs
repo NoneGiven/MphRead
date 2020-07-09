a0422527da7144f142e96222e65b39e7046d1a1f
None Given <none11given@gmail.com>
2020-07-09 16:53:24 -0400

    Cleanup.

--- src/MphRead/Renderer.cs
+++ src/MphRead/Renderer.cs
@@ -1867,7 +1867,7 @@ namespace MphRead
         {
             Model model = SelectedModel;
             await Output.Write(guid);
-            await Output.Write($"Camera ({_cameraPosition.X}, {_cameraPosition.Y}, {_cameraPosition.Z})", guid);
+            await Output.Write($"Camera ({_cameraPosition.X * -1}, {_cameraPosition.Y * -1}, {_cameraPosition.Z * -1})", guid);
             await Output.Write(guid);
             await Output.Write($"Model: {model.Name} [{model.SceneId}] {(model.Visible ? "On " : "Off")} - " +
                 $"Color {model.CurrentRecolor} / {model.Recolors.Count - 1}", guid);
