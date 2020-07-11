3baf723b9dcfc93ff24014783bb439be9167d121
None Given <none11given@gmail.com>
2020-07-11 19:50:41 -0400

    Add Python script.

---
+++ src/MphRead/Export/Python/mph_common.py
@@ -0,0 +1,193 @@
+import bpy
+
+def get_node(name):
+    return bpy.data.objects[name]
+
+def get_material(name):
+        return bpy.data.materials[name]
+
+def get_materials():
+    for item in bpy.data.materials:
+        yield bpy.data.materials[item.name]
+
+def material_get_node(self, name):
+    try:
+        return self.node_tree.nodes[name]
+    except:
+        return None
+
+def material_get_bsdf(self):
+    return self.get_node('Principled BSDF')
+
+def node_get_input(self, name):
+    return self.inputs[name]
+
+def node_get_output(self, name):
+    return self.outputs[name]
+
+def material_get_bsdf_input(self, name):
+    return self.get_bsdf().get_input(name)
+
+def material_add_node(self, kind):
+    return self.node_tree.nodes.new(kind)
+
+def material_link_nodes(self, source, output, target, input):
+    if (isinstance(source, str)):
+        source = self.get_node(source)
+    if (isinstance(target, str)):
+        target = self.get_node(target)
+    self.node_tree.links.new(
+        source.outputs[output],
+        target.inputs[input]
+    )
+
+bpy.types.Node.get_input = node_get_input;
+bpy.types.Node.get_output = node_get_output;
+bpy.types.Material.get_node = material_get_node;
+bpy.types.Material.get_bsdf = material_get_bsdf;
+bpy.types.Material.get_bsdf_input = material_get_bsdf_input;
+bpy.types.Material.link_nodes = material_link_nodes;
+bpy.types.Material.add_node = material_add_node;
+
+def set_common():
+    bpy.data.materials.remove(get_material('Dots Stroke'))
+    bpy.data.materials.remove(get_material('Material'))
+    for material in get_materials():
+        material.get_bsdf_input('Specular').default_value = 0
+        material.shadow_method = 'NONE'
+        set_vertex_colors(material)
+        
+def set_vertex_colors(material):
+    color = material.add_node('ShaderNodeVertexColor')
+    texture = material.get_node('Image Texture')
+    if (texture):
+        texture.interpolation = 'Closest'
+        mix = material.add_node('ShaderNodeMixRGB')
+        mix.blend_type = 'MULTIPLY'
+        mix.get_input('Fac').default_value = 1
+        material.link_nodes(
+            'Vertex Color', 'Color',
+            'Mix', 'Color1'
+        )
+        material.link_nodes(
+            'Image Texture', 'Color',
+            'Mix', 'Color2'
+        )
+        material.link_nodes(
+            'Mix', 'Color',
+            'Principled BSDF', 'Base Color'
+        )
+    else:
+        material.link_nodes(
+            'Vertex Color', 'Color',
+            'Principled BSDF', 'Base Color'
+        )
+    
+def set_material_alpha(name, alpha):
+    material = get_material(name)
+    material.blend_method = 'BLEND'
+    material.show_transparent_back = False
+    material.get_bsdf_input('Alpha').default_value = alpha / 31
+    
+def set_texture_alpha(name, alpha):
+    material = get_material(name)
+    material.blend_method = 'BLEND'
+    material.show_transparent_back = False
+    if (alpha == 31):
+        material.link_nodes(
+            'Image Texture', 'Alpha',
+            'Principled BSDF', 'Alpha'
+        )
+    else:
+        math = material.add_node('ShaderNodeMath')
+        math.operation = 'MULTIPLY'
+        math.inputs[1].default_value = alpha / 31
+        material.link_nodes(
+            'Image Texture', 'Alpha',
+            'Math', 0
+        )
+        material.link_nodes(
+            'Math', 'Value',
+            'Principled BSDF', 'Alpha'
+        )
+        
+def set_billboard(name):
+    node = get_node(name)
+    constraint = node.constraints.new('LOCKED_TRACK')
+    constraint.target = bpy.data.objects['Camera']
+    constraint.track_axis = 'TRACK_Z'
+    constraint.lock_axis = 'LOCK_Y'
+    constraint = node.constraints.new('LOCKED_TRACK')
+    constraint.target = bpy.data.objects['Camera']
+    constraint.track_axis = 'TRACK_Z'
+    constraint.lock_axis = 'LOCK_X'
+    
+def set_back_culling(name):
+    material = get_material(name)
+    material.use_backface_culling = True
+    
+def invert_normals(name):
+    if (bpy.context.object.mode != 'OBJECT'):
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.active_object.select_set(False)
+    for obj in bpy.context.selected_objects:
+        bpy.context.view_layer.objects.active = None
+    node = get_node(name)
+    bpy.context.view_layer.objects.active = node
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    bpy.ops.mesh.select_all(action = 'SELECT')
+    bpy.ops.mesh.flip_normals()
+    bpy.ops.mesh.select_all(action='DESELECT')
+    
+def set_mirror(name, x, y):
+    material = get_material(name)
+    uvs = material.add_node('ShaderNodeUVMap')
+    separate = material.add_node('ShaderNodeSeparateXYZ')
+    material.link_nodes(
+        uvs, 'UV',
+        separate, 'Vector'
+    )
+    combine = material.add_node('ShaderNodeCombineXYZ')
+    if (x):
+        math = material.add_node('ShaderNodeMath')
+        math.operation = 'PINGPONG'
+        math.inputs[1].default_value = 1
+        material.link_nodes(
+            separate, 'X',
+            math, 0
+        )
+        material.link_nodes(
+            math, 'Value',
+            combine, 'X'
+        )
+    else:
+        material.link_nodes(
+            separate, 'X',
+            combine, 'X'
+        )
+    if (y):
+        math = material.add_node('ShaderNodeMath')
+        math.operation = 'PINGPONG'
+        math.inputs[1].default_value = 1
+        material.link_nodes(
+            separate, 'Y',
+            math, 0
+        )
+        material.link_nodes(
+            math, 'Value',
+            combine, 'Y'
+        )
+    else:
+        material.link_nodes(
+            separate, 'Y',
+            combine, 'Y'
+        )
+    material.link_nodes(
+        separate, 'Z',
+        combine, 'Z'
+    )
+    material.link_nodes(
+        combine, 'Vector',
+        'Image Texture', 'Vector'
+    )
--- src/MphRead/Export/Scripting.cs
+++ src/MphRead/Export/Scripting.cs
@@ -6,11 +6,19 @@ namespace MphRead.Export
 {
     public class Scripting
     {
-        // todo: mph_common, auto-import + script running
+        // todo: auto-import + script running
         public static string GenerateScript(Model model)
         {
             var sb = new StringBuilder();
-            sb.AppendLine("import mph_common");
+            void AppendIndent()
+            {
+                sb!.Append(' ', 4);
+            }
+            sb.AppendLine("from mph_common import *");
+            sb.AppendLine();
+            sb.AppendLine("if __name__ == '__main__':");
+            sb.AppendLine();
+            AppendIndent();
             sb.AppendLine("set_common()");
             var invertMeshIds = new HashSet<int>();
             for (int i = 0; i < model.Materials.Count; i++)
@@ -18,20 +26,24 @@ namespace MphRead.Export
                 Material material = model.Materials[i];
                 if (material.TextureId != UInt16.MaxValue)
                 {
+                    AppendIndent();
                     sb.AppendLine($"set_texture_alpha('{material.Name}_mat', {material.Alpha})");
                     bool mirrorX = material.XRepeat == RepeatMode.Mirror;
                     bool mirrorY = material.YRepeat == RepeatMode.Mirror;
                     if (mirrorX || mirrorY)
                     {
+                        AppendIndent();
                         sb.AppendLine($"set_mirror('{material.Name}_mat', {(mirrorX ? "True" : "False")}, {(mirrorY ? "True" : "False")})");
                     }
                 }
                 else if (material.Alpha < 31)
                 {
+                    AppendIndent();
                     sb.AppendLine($"set_material_alpha('{material.Name}_mat', {material.Alpha})");
                 }
                 if (material.Culling == CullingMode.Back || material.Culling == CullingMode.Front)
                 {
+                    AppendIndent();
                     sb.AppendLine($"set_back_culling('{material.Name}_mat')");
                     if (material.Culling == CullingMode.Front)
                     {
@@ -50,12 +62,14 @@ namespace MphRead.Export
             {
                 if (node.Billboard)
                 {
+                    AppendIndent();
                     sb.AppendLine($"set_billboard('{node.Name}')");
                 }
                 foreach (int meshId in node.GetMeshIds())
                 {
                     if (invertMeshIds.Contains(meshId))
                     {
+                        AppendIndent();
                         if (node.MeshCount == 1)
                         {
                             sb.AppendLine($"invert_normals('{node.Name}')");
