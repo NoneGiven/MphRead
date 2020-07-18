5c48b7374c1dfef98262dff35122cf233ad26c3b
None Given <none11given@gmail.com>
2020-07-18 01:21:52 -0400

    Cleanup.

--- src/MphRead/SceneSetup.cs
+++ src/MphRead/SceneSetup.cs
@@ -504,6 +504,7 @@ namespace MphRead
             return model;
         }
 
+        // todo: AlimbicThinDoor needs to recolor the model, not the glow
         private static Model LoadDoor(DoorEntityData data)
         {
             string modelName = Metadata.Doors[(int)data.ModelId];
