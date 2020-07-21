89e1ad4acf9609471e6020b6b0074d5735c74fb4
None Given <none11given@gmail.com>
2020-07-20 22:00:18 -0400

    Cleanup.

--- src/MphRead/Renderer.cs
+++ src/MphRead/Renderer.cs
@@ -447,7 +447,7 @@ namespace MphRead
             }
             base.OnRenderFrame(args);
         }
-        
+
         private void SetSelectedModel(uint sceneId)
         {
             Deselect();
