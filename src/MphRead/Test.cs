bcbe6d4dd5ea04887cfed7edd92e2af57dd0ad74
None Given <none11given@gmail.com>
2020-07-23 23:40:22 -0400

    Cleanup.

--- src/MphRead/Test.cs
+++ src/MphRead/Test.cs
@@ -9,6 +9,22 @@ namespace MphRead
     {
 #pragma warning disable IDE0051 // Remove unused private members
 
+        private static int LeadingZeroes(int x)
+        {
+            const int numIntBits = sizeof(int) * 8;
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            x -= x >> 1 & 0x55555555;
+            x = (x >> 2 & 0x33333333) + (x & 0x33333333);
+            x = (x >> 4) + x & 0x0f0f0f0f;
+            x += x >> 8;
+            x += x >> 16;
+            return numIntBits - (x & 0x0000003f);
+        }
+
         public static void TestCollision()
         {
             ushort headerSize = 0;
