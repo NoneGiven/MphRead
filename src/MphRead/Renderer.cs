8c8dea2fcf7f7f73e1644abf125a23014a1bc5ce
None Given <none11given@gmail.com>
2020-08-09 23:13:30 -0400

    Cleanup.

--- src/MphRead/Renderer.cs
+++ src/MphRead/Renderer.cs
@@ -2385,7 +2385,7 @@ namespace MphRead
             await Output.Write($"Texture ID {material.CurrentTextureId}, Palette ID {material.CurrentPaletteId}", guid);
             await Output.Write($"Diffuse ({material.Diffuse.Red}, {material.Diffuse.Green}, {material.Diffuse.Blue})" +
                 $" Ambient ({material.Ambient.Red}, {material.Ambient.Green}, {material.Ambient.Blue})" +
-                $" Specular({ material.Specular.Red}, { material.Specular.Green}, { material.Specular.Blue})", guid);
+                $" Specular ({ material.Specular.Red}, { material.Specular.Green}, { material.Specular.Blue})", guid);
             await Output.Write(guid);
         }
 
