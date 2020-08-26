2a91aad13146027928961dd55eb40ccec00b695e
None Given <none11given@gmail.com>
2020-08-26 01:48:08 -0400

    Cleanup.

--- src/MphRead/Formats/Entity.cs
+++ src/MphRead/Formats/Entity.cs
@@ -553,7 +553,7 @@ namespace MphRead
         public readonly ushort Field6E;
         public readonly ushort Field70;
         public readonly ushort Field72;
-        public readonly ushort ActivationFlags; // in-game this is treated as uint, but the extra bits are never set/checked
+        public readonly ushort TriggerFlags; // in-game this is treated as uint, but the extra bits are never set/checked
         public readonly ushort Padding2;
         public readonly uint Field78;
         public readonly ushort PreviousId;
