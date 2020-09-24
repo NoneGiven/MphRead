5cd5c3b9db8fc28276292d4fadeb3571956f0483
None Given <none11given@gmail.com>
2020-09-24 17:02:19 -0400

    Bump version.

--- src/MphRead/Program.cs
+++ src/MphRead/Program.cs
@@ -7,7 +7,7 @@ namespace MphRead
 {
     internal static class Program
     {
-        public static Version Version { get; } = new Version(0, 9, 0, 0);
+        public static Version Version { get; } = new Version(0, 10, 0, 0);
 
         private static void Main(string[] args)
         {
