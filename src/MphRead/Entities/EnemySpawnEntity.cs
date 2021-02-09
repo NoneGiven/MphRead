49ce61f58ef6f648a1327b28124d4cde80022931
None Given <none11given@gmail.com>
2021-02-09 03:00:59 -0500

    Add comment.

--- src/MphRead/Entities/EnemySpawnEntity.cs
+++ src/MphRead/Entities/EnemySpawnEntity.cs
@@ -11,6 +11,7 @@ namespace MphRead.Entities
 
         public EnemyEntityData Data => _data;
 
+        // todo: enemy and item spawners should preload the models that will be used when they spawn their entities
         public EnemySpawnEntity(EnemyEntityData data) : base(NewEntityType.Enemy)
         {
             _data = data;
