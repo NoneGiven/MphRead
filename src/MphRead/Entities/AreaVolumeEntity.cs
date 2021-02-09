fac95cc5d37fc80ffe61e23ec6c38cd7805cc62c
None Given <none11given@gmail.com>
2021-02-08 22:55:32 -0500

    Add area and trigger volume display.

--- src/MphRead/Entities/AreaVolumeEntity.cs
+++ src/MphRead/Entities/AreaVolumeEntity.cs
@@ -7,12 +7,28 @@ namespace MphRead.Entities
         private readonly AreaVolumeEntityData _data;
         protected override Vector4? OverrideColor { get; } = new ColorRgb(0xFF, 0xFF, 0x00).AsVector4();
 
+        private readonly CollisionVolume _volume;
+        private readonly Vector3 _insideEventColor;
+        private readonly Vector3 _exitEventColor;
+
         public AreaVolumeEntity(AreaVolumeEntityData data) : base(NewEntityType.PlayerSpawn)
         {
             _data = data;
             Id = data.Header.EntityId;
+            _insideEventColor = Metadata.GetEventColor(data.InsideEvent);
+            _exitEventColor = Metadata.GetEventColor(data.ExitEvent);
             ComputeTransform(data.Header.RightVector, data.Header.UpVector, data.Header.Position);
+            _volume = SceneSetup.TransformVolume(_data.Volume, Transform);
             UsePlaceholderModel();
+        }
+
+        public override void GetDisplayVolumes(NewScene scene)
+        {
+            if (scene.ShowVolumes == VolumeDisplay.AreaInside || scene.ShowVolumes == VolumeDisplay.AreaExit)
+            {
+                Vector3 color = scene.ShowVolumes == VolumeDisplay.AreaInside ? _insideEventColor : _exitEventColor;
+                AddVolumeItem(_volume, color, scene);
+            }
         }
     }
 
@@ -21,12 +37,28 @@ namespace MphRead.Entities
         private readonly FhAreaVolumeEntityData _data;
         protected override Vector4? OverrideColor { get; } = new ColorRgb(0xFF, 0xFF, 0x00).AsVector4();
 
+        private readonly CollisionVolume _volume;
+        private readonly Vector3 _insideEventColor;
+        private readonly Vector3 _exitEventColor;
+
         public FhAreaVolumeEntity(FhAreaVolumeEntityData data) : base(NewEntityType.PlayerSpawn)
         {
             _data = data;
             Id = data.Header.EntityId;
+            _insideEventColor = Metadata.GetEventColor(data.InsideEvent);
+            _exitEventColor = Metadata.GetEventColor(data.ExitEvent);
             ComputeTransform(data.Header.RightVector, data.Header.UpVector, data.Header.Position);
+            _volume = SceneSetup.TransformVolume(_data.ActiveVolume, Transform);
             UsePlaceholderModel();
+        }
+
+        public override void GetDisplayVolumes(NewScene scene)
+        {
+            if (scene.ShowVolumes == VolumeDisplay.AreaInside || scene.ShowVolumes == VolumeDisplay.AreaExit)
+            {
+                Vector3 color = scene.ShowVolumes == VolumeDisplay.AreaInside ? _insideEventColor : _exitEventColor;
+                AddVolumeItem(_volume, color, scene);
+            }
         }
     }
 }
--- src/MphRead/Entities/TriggerVolumeEntity.cs
+++ src/MphRead/Entities/TriggerVolumeEntity.cs
@@ -7,12 +7,29 @@ namespace MphRead.Entities
         private readonly TriggerVolumeEntityData _data;
         protected override Vector4? OverrideColor { get; } = new ColorRgb(0xFF, 0x8C, 0x00).AsVector4();
 
+        private readonly CollisionVolume _volume;
+        private readonly Vector3 _parentEventColor;
+        private readonly Vector3 _childEventColor;
+
         public TriggerVolumeEntity(TriggerVolumeEntityData data) : base(NewEntityType.PlayerSpawn)
         {
             _data = data;
             Id = data.Header.EntityId;
+            // todo: change the display/color when inactive (same for AreaVolumes)
+            _parentEventColor = Metadata.GetEventColor(data.ParentEvent);
+            _childEventColor = Metadata.GetEventColor(data.ChildEvent);
             ComputeTransform(data.Header.RightVector, data.Header.UpVector, data.Header.Position);
+            _volume = SceneSetup.TransformVolume(_data.Volume, Transform);
             UsePlaceholderModel();
+        }
+
+        public override void GetDisplayVolumes(NewScene scene)
+        {
+            if (scene.ShowVolumes == VolumeDisplay.TriggerParent || scene.ShowVolumes == VolumeDisplay.TriggerChild)
+            {
+                Vector3 color = scene.ShowVolumes == VolumeDisplay.TriggerParent ? _parentEventColor : _childEventColor;
+                AddVolumeItem(_volume, color, scene);
+            }
         }
     }
 
@@ -21,12 +38,28 @@ namespace MphRead.Entities
         private readonly FhTriggerVolumeEntityData _data;
         protected override Vector4? OverrideColor { get; } = new ColorRgb(0xFF, 0x8C, 0x00).AsVector4();
 
+        private readonly CollisionVolume _volume;
+        private readonly Vector3 _parentEventColor;
+        private readonly Vector3 _childEventColor;
+
         public FhTriggerVolumeEntity(FhTriggerVolumeEntityData data) : base(NewEntityType.PlayerSpawn)
         {
             _data = data;
             Id = data.Header.EntityId;
+            _parentEventColor = Metadata.GetEventColor(data.ParentEvent);
+            _childEventColor = Metadata.GetEventColor(data.ChildEvent);
             ComputeTransform(data.Header.RightVector, data.Header.UpVector, data.Header.Position);
+            _volume = SceneSetup.TransformVolume(_data.ActiveVolume, Transform);
             UsePlaceholderModel();
+        }
+
+        public override void GetDisplayVolumes(NewScene scene)
+        {
+            if (scene.ShowVolumes == VolumeDisplay.TriggerParent || scene.ShowVolumes == VolumeDisplay.TriggerChild)
+            {
+                Vector3 color = scene.ShowVolumes == VolumeDisplay.TriggerParent ? _parentEventColor : _childEventColor;
+                AddVolumeItem(_volume, color, scene);
+            }
         }
     }
 }
