7c4c2abd2dc7ccd7cdc74651ce8be326251c6252
None Given <none11given@gmail.com>
2021-02-11 18:18:39 -0500

    Spawn effect for artifact key.

--- src/MphRead/Entities/ItemInstanceEntity.cs
+++ src/MphRead/Entities/ItemInstanceEntity.cs
@@ -1,3 +1,4 @@
+using MphRead.Effects;
 using OpenTK.Mathematics;
 
 namespace MphRead.Entities
@@ -17,11 +18,40 @@ namespace MphRead.Entities
 
     public class ItemInstanceEntity : SpinningEntityBase
     {
+        public int ItemType { get; }
+        private EffectEntry? _effectEntry = null;
+        private const int _effectId = 144; // artifactKeyEffect
+
         public ItemInstanceEntity(ItemInstanceEntityData data) : base(0.35f, Vector3.UnitY, 0, 0, EntityType.ItemInstance)
         {
             Position = data.Position.AddY(0.65f);
             ModelInstance inst = Read.GetModelInstance(Metadata.Items[data.ItemType]);
             _models.Add(inst);
+            ItemType = data.ItemType;
+        }
+
+        public override void Initialize(Scene scene)
+        {
+            base.Initialize(scene);
+            if (ItemType == 19) // Artifact_Key
+            {
+                scene.LoadEffect(_effectId);
+                Matrix4 transform = Matrix.GetTransform4(Vector3.UnitX, Vector3.UnitY, Position);
+                _effectEntry = scene.SpawnEffectGetEntry(_effectId, transform);
+                for (int i = 0; i < _effectEntry.Elements.Count; i++)
+                {
+                    EffectElementEntry element = _effectEntry.Elements[i];
+                    element.Flags |= 0x80000; // set bit 19 (lifetime extension)
+                }
+            }
+        }
+
+        public override void Destroy(Scene scene)
+        {
+            if (_effectEntry != null)
+            {
+                scene.UnlinkEffectEntry(_effectEntry);
+            }
         }
     }
 
