7afd4be3332125f953f560378f972a97a19442d9
None Given <none11given@gmail.com>
2021-02-17 23:11:50 -0500

    Add comment.

--- src/MphRead/Entities/Enemies/CarnivorousPlant.cs
+++ src/MphRead/Entities/Enemies/CarnivorousPlant.cs
@@ -8,6 +8,7 @@ namespace MphRead.Entities.Enemies
             _initialPosition = Position;
             var spawner = (EnemySpawnEntity)data.Spawner;
             ObjectMetadata meta = Metadata.GetObjectById(spawner.Data.TextureId);
+            // todo: enemy spawners need to load these initially
             ModelInstance inst = Read.GetModelInstance(meta.Name);
             _models.Add(inst);
         }
