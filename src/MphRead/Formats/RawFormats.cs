80eea22ca524ce4fab94b94482dadae53ffb5383
None Given <none11given@gmail.com>
2021-03-11 20:36:19 -0500

    Cleanup.

--- src/MphRead/Formats/RawFormats.cs
+++ src/MphRead/Formats/RawFormats.cs
@@ -108,7 +108,7 @@ namespace MphRead
         public readonly ushort TextureIdCount;
         public readonly ushort PaletteIdCount;
         public readonly ushort AnimationCount;
-        public readonly ushort FieldA;
+        public readonly ushort PaddingA;
         public readonly uint FrameIndexOffset;
         public readonly uint TextureIdOffset;
         public readonly uint PaletteIdOffset;
