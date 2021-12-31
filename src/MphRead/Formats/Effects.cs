8407f2f497626607a197eaa68b5d3333c4cb3c41
None Given <none11given@gmail.com>
2021-12-30 22:39:18 -0500

    Fix scripting issues.

--- src/MphRead/Formats/Effects.cs
+++ src/MphRead/Formats/Effects.cs
@@ -3,7 +3,6 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography;
 using MphRead.Entities;
 using OpenTK.Mathematics;
 
