e5154a317ec938fea654a851eb3f34cd6e708c0b
None Given <none11given@gmail.com>
2022-01-09 13:56:43 -0500

    More cleanup.

--- src/MphRead/Entities/Enemies/10_BarbedWarWasp.cs
+++ src/MphRead/Entities/Enemies/10_BarbedWarWasp.cs
@@ -44,20 +44,20 @@ namespace MphRead.Entities.Enemies
         // todo: this is mostly identical to War Wasp
         protected override bool EnemyInitialize()
         {
-            Recolor = (int)_spawner.Data.Fields.S08.EnemyVersion;
+            Recolor = (int)_spawner.Data.Fields.S08.EnemySubtype;
             SetTransform(_spawner.FacingVector, Vector3.UnitY, _spawner.Position);
             _movementType = _spawner.Data.Fields.S08.WarWasp.MovementType;
             Flags |= EnemyFlags.Visible;
             Flags |= EnemyFlags.OnRadar;
             _boundingRadius = 1;
             _hurtVolumeInit = new CollisionVolume(new Vector3(0, -0.45f, 0), 1.4f);
-            _values = Metadata.Enemy10Values[(int)_spawner.Data.Fields.S08.EnemySubtype];
+            _values = Metadata.Enemy10Values[Recolor];
             _health = _healthMax = _values.HealthMax;
             Metadata.LoadEffectiveness(_values.Effectiveness, BeamEffectiveness);
             // todo: scan ID
             _homeVolume = CollisionVolume.Move(_spawner.Data.Fields.S08.WarWasp.Volume2, Position);
             _movementVolume = CollisionVolume.Move(_spawner.Data.Fields.S08.WarWasp.Volume1, Position);
-            WeaponInfo weapon = Weapons.EnemyWeapons[Recolor];
+            WeaponInfo weapon = Weapons.EnemyWeapons[(int)_spawner.Data.Fields.S08.EnemyVersion];
             weapon.UnchargedDamage = _values.BeamDamage;
             weapon.SplashDamage = _values.SplashDamage;
             _equipInfo = new EquipInfo(weapon, _beams);
