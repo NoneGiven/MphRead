fc0cf7809b655b5de1734327f0f1675bb5276c62
None Given <none11given@gmail.com>
2022-02-08 09:53:31 -0500

    Fix cam seq and fade bugs.

--- src/MphRead/SceneSetup.cs
+++ src/MphRead/SceneSetup.cs
@@ -66,7 +66,9 @@ namespace MphRead
                 nodeData = ReadNodeData.ReadData(Path.Combine(@"", metadata.NodePath));
             }
             LoadResources(scene);
+            CamSeqEntity.Current = null;
             CameraSequence.Current = null;
+            CameraSequence.Intro = null;
             if (scene.Multiplayer && PlayerEntity.PlayerCount > 0)
             {
                 int seqId = roomId - 93 + 172;
