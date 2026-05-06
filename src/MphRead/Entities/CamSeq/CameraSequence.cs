90688dd60bd4420662e70e8882e8c43c12be37b3
None Given <none11given@gmail.com>
2026-05-06 01:04:06 -0400

    Code cleanup.

--- src/MphRead/Entities/CamSeq/CameraSequence.cs
+++ src/MphRead/Entities/CamSeq/CameraSequence.cs
@@ -47,7 +47,7 @@ namespace MphRead.Formats
         public static CameraSequence? Current { get; set; }
         public static CameraSequence? Intro { get; set; }
         public bool IsIntro => SequenceId >= 172 && SequenceId <= 198;
-        private static readonly FrozenSet<int> _cockpitLoops = [ 102, 103, 104, 105, 106, 168 ];
+        private static readonly FrozenSet<int> _cockpitLoops = [102, 103, 104, 105, 106, 168];
 
         private CameraSequence(int id, string name, Scene scene,
             CameraSequenceHeader header, IReadOnlyList<RawCameraSequenceKeyframe> keyframes)
--- src/MphRead/Selection.cs
+++ src/MphRead/Selection.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using MphRead.Entities;
 using OpenTK.Mathematics;
